ff1d256efbe4b6d30dbda33cb97899f3f3ef984d
Qiang Fu <fuqcool@gmail.com>
2014-03-01 21:04:45 -0500

    changed api name
    $.fn.xclick.config -> $.fn.xclick.on

--- xclick.js
+++ xclick.js
@@ -1,32 +1,31 @@
 (function ($) {
   var CLASS_LOCKED = 'locked';
 
-  var configList = [];
+  var events = [];
 
   function async(handler) {
     return function (evt) {
       var $el = $(this);
-      var config = getConfig(this);
+      var callbacks = getCallbacks(this);
 
       // return if element is locked
       if ($el.hasClass(CLASS_LOCKED)) return;
 
       $el.addClass(CLASS_LOCKED);
-      config.disable(this);
+      callbacks.disable.call(this, this);
 
       var done = $.proxy(_done, this);
 
       // add done to clickHandler, invoke done to unclock element
       handler.call(this, evt, done);
 
-
       function _done(cb) {
         // unlock element when done is invoked
         $el.removeClass(CLASS_LOCKED);
-        config.enable(this);
+        callbacks.enable.call(this, this);
 
         if ($.isFunction(cb)) {
-          cb(this);
+          cb.call(this, this);
         }
       }
     };
@@ -37,28 +36,29 @@
     this.click(async(handler));
   };
 
-  function getConfig(el) {
+  function getCallbacks(el) {
     var $el = $(el);
-    var config;
-
+    var event;
     var i;
-    for (i = 0; i < configList.length; i++) {
-      config = configList[i];
 
-      if ($el.is(config.el)) {
-        return config;
+    for (i = 0; i < events.length; i++) {
+      event = events[i];
+
+      if ($el.is(event.el)) {
+        return event;
       }
     }
 
+    // return a stub if callbacks not found
     return {
       enable: $.noop,
       disable: $.noop
     };
   }
 
-  $.fn.xclick.config = function (options) {
+  $.fn.xclick.on = function (options) {
     if (options) {
-      configList.push({
+      events.push({
         el: options.el,
         enable: options.enable || $.noop,
         disable: options.disable || $.noop
